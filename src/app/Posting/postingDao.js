31260d5652ba6a549794024a8a874ae59f338aed
great-park <cksgh1735@naver.com>
2022-03-22 18:00:01 +0900

    🚧 게시글 상세페이지 API version 2

--- src/app/Posting/postingDao.js
+++ src/app/Posting/postingDao.js
@@ -1,69 +1,69 @@
 // 게시글 생성
 async function createPosting(connection, insertPostingParams) {
-    const insertPostingQuery = `
+  const insertPostingQuery = `
 INSERT INTO Posting(postUserId, title, gatheringTime, runningTime, gatherLongitude, gatherLatitude, locationInfo, runningTag, ageMin, ageMax, peopleNum, contents, runnerGender)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
                `;
-    const insertPostingRow = await connection.query(
-        insertPostingQuery,
-        insertPostingParams
-    );
+  const insertPostingRow = await connection.query(
+    insertPostingQuery,
+    insertPostingParams
+  );
 
-    return insertPostingRow;
+  return insertPostingRow;
 }
 
 // 러닝모임 생성
 async function createRunning(connection, createRunningParams) {
-    const createRunningQuery = `
+  const createRunningQuery = `
               INSERT INTO Running(repUserId, postId) VALUES (?,?);
                  `;
-    const createRunningRow = await connection.query(
-        createRunningQuery,
-        createRunningParams
-    );
+  const createRunningRow = await connection.query(
+    createRunningQuery,
+    createRunningParams
+  );
 
-    return createRunningRow;
+  return createRunningRow;
 }
 
 // 러닝 당 people에 반장 추가
 async function createRunningPeople(connection, insertRunningPeopleParams) {
-    const createRunningPeopleQuery = `
+  const createRunningPeopleQuery = `
                 INSERT INTO RunningPeople(gatheringId, userId, whetherAccept) VALUES (?,?,'Y');
                  `;
-    const createRunningPeopleRow = await connection.query(
-        createRunningPeopleQuery,
-        insertRunningPeopleParams
-    );
+  const createRunningPeopleRow = await connection.query(
+    createRunningPeopleQuery,
+    insertRunningPeopleParams
+  );
 
-    return createRunningPeopleRow;
+  return createRunningPeopleRow;
 }
 // 유저 있는지 확인
 async function userIdCheck(connection, userId) {
-    const findUserQuery = `
+  const findUserQuery = `
               SELECT userId FROM User WHERE userId = ?
                  `;
-    const findUserRow = await connection.query(findUserQuery, userId);
+  const findUserRow = await connection.query(findUserQuery, userId);
 
-    return findUserRow;
+  return findUserRow;
 }
 
 // 작성자인지 확인
 async function checkWriter(connection, checkWriterParams) {
-    const checkWriterQuery = `
+  const checkWriterQuery = `
               SELECT postId, postUserId FROM Posting
               where postId = ? and postUserId = ?;
                  `;
-    const checkWriterRow = await connection.query(
-        checkWriterQuery,
-        checkWriterParams
-    );
+  const checkWriterRow = await connection.query(
+    checkWriterQuery,
+    checkWriterParams
+  );
 
-    return checkWriterRow;
+  return checkWriterRow;
 }
 
 //게시글 상세페이지
 async function getPosting(connection, postId) {
-    const getPostingQuery = `
+  const getPostingQuery = `
 SELECT P.postId, P.createdAt as postingTime, postUserId,
      case when runningTag = 'A'
          then '퇴근 후'
@@ -88,14 +88,14 @@ INNER JOIN User U on U.userId = P.postUserId
 INNER JOIN Running R on R.postId = P.postId
 WHERE P.postId = ?;
                  `;
-    const getPostingRow = await connection.query(getPostingQuery, postId);
+  const getPostingRow = await connection.query(getPostingQuery, postId);
 
-    return getPostingRow;
+  return getPostingRow;
 }
 
 //해당 모임에 참여하는 러너 정보 가져오기
 async function getRunner(connection, postId) {
-    const getRunnerQuery = `
+  const getRunnerQuery = `
       SELECT U.userId, nickName,
              case when gender='M' then '남성'
                   else case when gender='F' then '여성'
@@ -134,13 +134,13 @@ async function getRunner(connection, postId) {
       inner join Running R on RP.gatheringId = R.gatheringId
       WHERE postId = ? AND whetherAccept = 'Y';
                  `;
-    const getRunnerRow = await connection.query(getRunnerQuery, postId);
+  const getRunnerRow = await connection.query(getRunnerQuery, postId);
 
-    return getRunnerRow;
+  return getRunnerRow;
 }
 // 신청하고 수락 대기중인 러너들
 async function getWaitingRunner(connection, postId) {
-    const getRunnerQuery = `
+  const getRunnerQuery = `
       SELECT U.userId, nickName,
              case when gender='M' then '남성'
                   else case when gender='F' then '여성'
@@ -179,90 +179,127 @@ async function getWaitingRunner(connection, postId) {
       inner join Running R on RP.gatheringId = R.gatheringId
       WHERE postId = ? AND whetherAccept = 'N';
                  `;
-    const getWaitingRunnerRow = await connection.query(getRunnerQuery, postId);
+  const getWaitingRunnerRow = await connection.query(getRunnerQuery, postId);
 
-    return getWaitingRunnerRow;
+  return getWaitingRunnerRow;
 }
 // 마감하기
 async function closePosting(connection, postId) {
-    const closePostingQuery = `
+  const closePostingQuery = `
   UPDATE Running SET whetherEnd = 'Y'
   WHERE postId = ?;
                  `;
-    const closePostingRow = await connection.query(closePostingQuery, postId);
+  const closePostingRow = await connection.query(closePostingQuery, postId);
 
-    return closePostingRow;
+  return closePostingRow;
 }
 
 // 게시글 수정
 async function patchPosting(connection, patchPostingParams) {
-    const patchPostingQuery = `
+  const patchPostingQuery = `
   UPDATE Posting SET title = ?, gatheringTime = ?, runningTime = ?, gatherLongitude = ?, gatherLatitude = ?, locationInfo = ?, runningTag = ?, ageMin = ?, ageMax = ?, 
                    peopleNum = ?, contents = ?, runnerGender = ?
   WHERE postId = ?;
                  `;
-    const patchPostingRow = await connection.query(
-        patchPostingQuery,
-        patchPostingParams
-    );
+  const patchPostingRow = await connection.query(
+    patchPostingQuery,
+    patchPostingParams
+  );
 
-    return patchPostingRow;
+  return patchPostingRow;
 }
 
 // 게시글 있는지 확인
 async function checkPosting(connection, postId) {
-    const checkPostingQuery = `
+  const checkPostingQuery = `
               SELECT postId FROM Posting WHERE postId = ?
                  `;
-    const checkPostingRow = await connection.query(checkPostingQuery, postId);
+  const checkPostingRow = await connection.query(checkPostingQuery, postId);
 
-    return checkPostingRow;
+  return checkPostingRow;
 }
 // 게시글 삭제
 async function dropPosting(connection, postId) {
-    const dropPostingQuery = `
+  const dropPostingQuery = `
               DELETE FROM Posting WHERE postId=?;
                  `;
-    const dropPostingRow = await connection.query(dropPostingQuery, postId);
+  const dropPostingRow = await connection.query(dropPostingQuery, postId);
 
-    return dropPostingRow;
+  return dropPostingRow;
 }
 
 // 찜 등록여부
 async function checkBookMark(connection, checkBookMarkParams) {
-    const checkBookMarkQuery = `
+  const checkBookMarkQuery = `
   SELECT userId FROM Bookmarks WHERE userId = ? AND postId = ?;
                  `;
-    const checkBookMarkRow = await connection.query(
-        checkBookMarkQuery,
-        checkBookMarkParams
-    );
+  const checkBookMarkRow = await connection.query(
+    checkBookMarkQuery,
+    checkBookMarkParams
+  );
 
-    return checkBookMarkRow;
+  return checkBookMarkRow;
 }
 
 // 신고하기
 async function reportPosting(connection, Params) {
-    const Query = `
+  const Query = `
   INSERT INTO AdminReport(postId, reporterId) VALUES (?,?);
                  `;
-    const Row = await connection.query(Query, Params);
+  const Row = await connection.query(Query, Params);
 
-    return Row;
+  return Row;
 }
+
+//게시글 상세페이지 v2
+async function getPosting2(connection, postId) {
+  const getPostingQuery = `
+  SELECT P.postId, P.createdAt as postingTime, postUserId, nickName, profileImageUrl,
+       title, runningTime, gatheringTime, gatherLongitude, gatherLatitude, locationInfo,
+       case when runningTag = 'A'
+           then '퇴근 후'
+        else case when runningTag = 'B'
+            then '출근 전'
+        else case when runningTag = 'H'
+            then '휴일'
+        end end end as runningTag,
+        concat(ageMin,'-',ageMax) as age,
+       case when runnerGender='A' then '전체'
+       else case when runnerGender='M' then '남성'
+       else case when runnerGender='F' then '여성'
+        end end end as gender,
+        whetherEnd, J.job,
+       peopleNum,
+       contents
+  FROM Posting P
+  INNER JOIN User U on U.userId = P.postUserId
+  INNER JOIN Running R on R.postId = P.postId
+  INNER JOIN (SELECT DISTINCT postId, GROUP_CONCAT(distinct(job)) as job
+  FROM RunningPeople RP
+  inner join Running R on RP.gatheringId = R.gatheringId
+  inner join User U on RP.userId = U.userId
+  group by postId) J on J.postId = P.postId
+  WHERE P.postId = ?;
+                   `;
+  const getPostingRow = await connection.query(getPostingQuery, postId);
+
+  return getPostingRow;
+}
+
 module.exports = {
-    createPosting,
-    userIdCheck,
-    createRunning,
-    createRunningPeople,
-    checkWriter,
-    getPosting,
-    getRunner,
-    getWaitingRunner,
-    closePosting,
-    patchPosting,
-    checkPosting,
-    dropPosting,
-    checkBookMark,
-    reportPosting,
+  createPosting,
+  userIdCheck,
+  createRunning,
+  createRunningPeople,
+  checkWriter,
+  getPosting,
+  getRunner,
+  getWaitingRunner,
+  closePosting,
+  patchPosting,
+  checkPosting,
+  dropPosting,
+  checkBookMark,
+  reportPosting,
+  getPosting2,
 };
