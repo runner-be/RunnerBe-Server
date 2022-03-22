0f02725626b21178d58085fd4b45d083dc3c23f9
great-park <cksgh1735@naver.com>
2022-03-22 17:59:31 +0900

    ✨ update

--- config/express.js
+++ config/express.js
@@ -3,24 +3,24 @@ const compression = require("compression");
 const methodOverride = require("method-override");
 var cors = require("cors");
 module.exports = function () {
-    const app = express();
+  const app = express();
 
-    app.use(compression());
+  app.use(compression());
 
-    app.use(express.json());
+  app.use(express.json());
 
-    app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true }));
 
-    app.use(methodOverride());
+  app.use(methodOverride());
 
-    app.use(cors());
-    // app.use(express.static(process.cwd() + '/public'));
+  app.use(cors());
+  // app.use(express.static(process.cwd() + '/public'));
 
-    /* App (Android, iOS) */
-    require("../src/app/User/userRoute")(app);
-    require("../src/app/Posting/postingRoute")(app);
-    require("../src/app/Message/messageRoute")(app);
-    require("../src/app/Running/runningRoute")(app);
+  /* App (Android, iOS) */
+  require("../src/app/User/userRoute")(app);
+  require("../src/app/Posting/postingRoute")(app);
+  require("../src/app/Message/messageRoute")(app);
+  require("../src/app/Running/runningRoute")(app);
 
-    return app;
+  return app;
 };
