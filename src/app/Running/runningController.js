8eff5f4405084b7045973eef156a4bc828ba2e8c
great-park <cksgh1735@naver.com>
2022-05-06 20:58:17 +0900

    ⚡️ Running의 route 수정

--- src/app/Running/runningController.js
+++ src/app/Running/runningController.js
@@ -14,7 +14,7 @@ const { initializeApp } = require("firebase-admin/app");
 const admin = require("firebase-admin");
 let serAccount = require("../../../config/runnerne-firebase-adminsdk-5jzt3-826315ed27.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serAccount),
+  credential: admin.credential.cert(serAccount),
 });
 
 /**
@@ -23,49 +23,48 @@ admin.initializeApp({
  * [POST] /running/request/:postId
  */
 exports.sendRequest = async function (req, res) {
-    /**
-     * Header : jwt
-     * Path Variable : postId
-     * query String : userId
-     */
-    const postId = req.params.postId;
-    const userId = req.query.userId;
-    const userIdFromJWT = req.verifiedToken.userId;
+  /**
+   * Header : jwt
+   * Path Variable : postId, userId
+   */
+  const postId = req.params.postId;
+  const userId = req.params.userId;
+  const userIdFromJWT = req.verifiedToken.userId;
 
-    // 필수 값 : 빈 값 체크 (text를 제외한 나머지)
-    if (!userId) return res.send(response(baseResponse.USER_USERID_EMPTY));
-    if (!postId) return res.send(response(baseResponse.POSTID_EMPTY));
+  // 필수 값 : 빈 값 체크 (text를 제외한 나머지)
+  if (!userId) return res.send(response(baseResponse.USER_USERID_EMPTY));
+  if (!postId) return res.send(response(baseResponse.POSTID_EMPTY));
 
-    // 숫자 확인
-    if (isNaN(userId) === true)
-        return res.send(response(baseResponse.USER_USERID_NOTNUM));
-    if (isNaN(postId) === true)
-        return res.send(response(baseResponse.POSTID_NOTNUM));
+  // 숫자 확인
+  if (isNaN(userId) === true)
+    return res.send(response(baseResponse.USER_USERID_NOTNUM));
+  if (isNaN(postId) === true)
+    return res.send(response(baseResponse.POSTID_NOTNUM));
 
-    // 이미 참여 신청을 했었는지 확인
-    const checkAlreadyapplyNotD = await messageProvider.checkAlreadyapplyNotD(
-        userId,
-        postId
-    );
-    if (checkAlreadyapplyNotD.length != 0) {
-        return res.send(response(baseResponse.ALREADY_APPLY));
+  // 이미 참여 신청을 했었는지 확인
+  const checkAlreadyapplyNotD = await messageProvider.checkAlreadyapplyNotD(
+    userId,
+    postId
+  );
+  if (checkAlreadyapplyNotD.length != 0) {
+    return res.send(response(baseResponse.ALREADY_APPLY));
+  }
+  //반장 userId
+
+  //jwt로 userId 확인
+  if (userIdFromJWT != userId) {
+    res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+  } else {
+    // 인증 대기 회원 확인
+    const checkUserAuth = await userProvider.checkUserAuth(userId);
+    if (checkUserAuth.length === 0) {
+      return res.send(response(baseResponse.USER_NON_AUTH));
     }
-    //반장 userId
+    const Response = await runningService.sendRequest(postId, userId);
+    //푸쉬알림
 
-    //jwt로 userId 확인
-    if (userIdFromJWT != userId) {
-        res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
-    } else {
-        // 인증 대기 회원 확인
-        const checkUserAuth = await userProvider.checkUserAuth(userId);
-        if (checkUserAuth.length === 0) {
-            return res.send(response(baseResponse.USER_NON_AUTH));
-        }
-        const Response = await runningService.sendRequest(postId, userId);
-        //푸쉬알림
-
-        return res.send(response(baseResponse.SUCCESS));
-    }
+    return res.send(response(baseResponse.SUCCESS));
+  }
 };
 
 /**
@@ -74,68 +73,67 @@ exports.sendRequest = async function (req, res) {
  * [PATCH] /running/request/:postId/handling/:applicantId/:whetherAccept
  */
 exports.handleRequest = async function (req, res) {
-    /**
-     * Header : jwt
-     * Path Variable : postId, applicantId, whetherAccept
-     * query String : userId
-     */
-    const postId = req.params.postId;
-    const applicantId = req.params.applicantId;
-    const whetherAccept = req.params.whetherAccept;
-    const userId = req.query.userId;
-    const userIdFromJWT = req.verifiedToken.userId;
+  /**
+   * Header : jwt
+   * Path Variable : postId, applicantId, whetherAccept, userId
+   */
+  const postId = req.params.postId;
+  const applicantId = req.params.applicantId;
+  const whetherAccept = req.params.whetherAccept;
+  const userId = req.params.userId;
+  const userIdFromJWT = req.verifiedToken.userId;
 
-    // 필수 값 : 빈 값 체크 (text를 제외한 나머지)
-    if (!userId) return res.send(response(baseResponse.USER_USERID_EMPTY));
-    if (!postId) return res.send(response(baseResponse.POSTID_EMPTY));
-    if (!applicantId) return res.send(response(baseResponse.APPLICANTID_EMPTY));
-    if (!whetherAccept) return res.send(response(baseResponse.WACCEPT_EMPTY));
+  // 필수 값 : 빈 값 체크 (text를 제외한 나머지)
+  if (!userId) return res.send(response(baseResponse.USER_USERID_EMPTY));
+  if (!postId) return res.send(response(baseResponse.POSTID_EMPTY));
+  if (!applicantId) return res.send(response(baseResponse.APPLICANTID_EMPTY));
+  if (!whetherAccept) return res.send(response(baseResponse.WACCEPT_EMPTY));
 
-    // 숫자 확인
-    if (isNaN(userId) === true)
-        return res.send(response(baseResponse.USER_USERID_NOTNUM));
-    if (isNaN(postId) === true)
-        return res.send(response(baseResponse.POSTID_NOTNUM));
-    if (isNaN(applicantId) === true)
-        return res.send(response(baseResponse.APPLICANTID_NOTNUM));
+  // 숫자 확인
+  if (isNaN(userId) === true)
+    return res.send(response(baseResponse.USER_USERID_NOTNUM));
+  if (isNaN(postId) === true)
+    return res.send(response(baseResponse.POSTID_NOTNUM));
+  if (isNaN(applicantId) === true)
+    return res.send(response(baseResponse.APPLICANTID_NOTNUM));
 
-    // 유효성 검사
-    const whetherAcceptList = ["Y", "D"];
-    if (!whetherAcceptList.includes(whetherAccept))
-        return res.send(response(baseResponse.WACCEPT_IS_NOT_VALID));
+  // 유효성 검사
+  const whetherAcceptList = ["Y", "D"];
+  if (!whetherAcceptList.includes(whetherAccept))
+    return res.send(response(baseResponse.WACCEPT_IS_NOT_VALID));
 
-    // 반장인지 확인
-    //repUserId 뽑아내기
-    const repUserId = await messageProvider.getRepUserId(postId);
-    // 비교
-    if (userId != repUserId)
-        return res.send(response(baseResponse.USERID_NOT_WRITER));
+  // 반장인지 확인
+  //repUserId 뽑아내기
+  const repUserId = await messageProvider.getRepUserId(postId);
+  // 비교
+  if (userId != repUserId)
+    return res.send(response(baseResponse.USERID_NOT_WRITER));
 
-    //applicantId로 RP에 있는지, 그리고 whetherAccept가 N(대기)인지 확인
-    const checkApplicant = await runningProvider.checkApplicant(
-        applicantId,
-        postId
-    );
-    if (checkApplicant.length === 0)
-        return res.send(response(baseResponse.USER_CANNOT_REQUEST));
+  //applicantId로 RP에 있는지, 그리고 whetherAccept가 N(대기)인지 확인
+  const checkApplicant = await runningProvider.checkApplicant(
+    applicantId,
+    postId
+  );
+  if (checkApplicant.length === 0)
+    return res.send(response(baseResponse.USER_CANNOT_REQUEST));
 
-    //jwt로 userId 확인
-    if (userIdFromJWT != userId) {
-        res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
-    } else {
-        // 인증 대기 회원 확인
-        const checkUserAuth = await userProvider.checkUserAuth(userId);
-        if (checkUserAuth.length === 0) {
-            return res.send(response(baseResponse.USER_NON_AUTH));
-        }
-        const Response = await runningService.handleRequest(
-            postId,
-            applicantId,
-            whetherAccept
-        );
-
-        return res.send(response(baseResponse.SUCCESS));
+  //jwt로 userId 확인
+  if (userIdFromJWT != userId) {
+    res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+  } else {
+    // 인증 대기 회원 확인
+    const checkUserAuth = await userProvider.checkUserAuth(userId);
+    if (checkUserAuth.length === 0) {
+      return res.send(response(baseResponse.USER_NON_AUTH));
     }
+    const Response = await runningService.handleRequest(
+      postId,
+      applicantId,
+      whetherAccept
+    );
+
+    return res.send(response(baseResponse.SUCCESS));
+  }
 };
 
 /**
@@ -144,35 +142,35 @@ exports.handleRequest = async function (req, res) {
  * [GET] /pushAlarm
  */
 exports.pushAlarm = async function (req, res) {
-    const { userId } = req.verifiedToken;
+  const { userId } = req.verifiedToken;
 
-    const getDeviceTokenRows = await runningDao.getDeviceToken(userId);
-    if (getDeviceToken.length === 0)
-        return res.send(response(baseResponse.DEVICE_TOKEN_EMPTY));
+  const getDeviceTokenRows = await runningDao.getDeviceToken(userId);
+  if (getDeviceToken.length === 0)
+    return res.send(response(baseResponse.DEVICE_TOKEN_EMPTY));
 
-    let message = {
-        notification: {
-            title: "테스트 제목",
-            body: getDeviceTokenRows[0].nickName + "님 러너비 앱을 확인해보세요!",
-        },
-        data: {
-            title: "테스트 제목",
-            body: getDeviceTokenRows[0].nickName + "님 러너비 앱을 확인해보세요!",
-        },
-        token: getDeviceTokenRows[0].deviceToken,
-    };
+  let message = {
+    notification: {
+      title: "테스트 제목",
+      body: getDeviceTokenRows[0].nickName + "님 러너비 앱을 확인해보세요!",
+    },
+    data: {
+      title: "테스트 제목",
+      body: getDeviceTokenRows[0].nickName + "님 러너비 앱을 확인해보세요!",
+    },
+    token: getDeviceTokenRows[0].deviceToken,
+  };
 
-    admin
-        .messaging()
-        .send(message)
-        .then(function (response) {
-            console.log("Successfully sent message: : ", response);
-            return res.send(response(baseResponse.SUCCESS));
-        })
-        .catch(function (err) {
-            console.log("Error Sending message!!! : ", err);
-            return res.send(response(baseResponse.ERROR_SEND_MESSAGE));
-        });
+  admin
+    .messaging()
+    .send(message)
+    .then(function (response) {
+      console.log("Successfully sent message: : ", response);
+      return res.send(response(baseResponse.SUCCESS));
+    })
+    .catch(function (err) {
+      console.log("Error Sending message!!! : ", err);
+      return res.send(response(baseResponse.ERROR_SEND_MESSAGE));
+    });
 };
 
 /**
@@ -181,44 +179,44 @@ exports.pushAlarm = async function (req, res) {
  * [PATCH] /runnings/:postId/attendees/:userId
  */
 exports.attend = async function (req, res) {
-    /**
-     * Header : jwt
-     * Path Variable : postId, userId
-     */
-    const postId = req.params.postId;
-    const userId = req.params.userId;
-    const userIdFromJWT = req.verifiedToken.userId;
+  /**
+   * Header : jwt
+   * Path Variable : postId, userId
+   */
+  const postId = req.params.postId;
+  const userId = req.params.userId;
+  const userIdFromJWT = req.verifiedToken.userId;
 
-    // 필수 값 : 빈 값 체크 (text를 제외한 나머지)
-    if (!userId) return res.send(response(baseResponse.USER_USERID_EMPTY));
-    if (!postId) return res.send(response(baseResponse.POSTID_EMPTY));
+  // 필수 값 : 빈 값 체크 (text를 제외한 나머지)
+  if (!userId) return res.send(response(baseResponse.USER_USERID_EMPTY));
+  if (!postId) return res.send(response(baseResponse.POSTID_EMPTY));
 
-    // 숫자 확인
-    if (isNaN(userId) === true)
-        return res.send(response(baseResponse.USER_USERID_NOTNUM));
-    if (isNaN(postId) === true)
-        return res.send(response(baseResponse.POSTID_NOTNUM));
+  // 숫자 확인
+  if (isNaN(userId) === true)
+    return res.send(response(baseResponse.USER_USERID_NOTNUM));
+  if (isNaN(postId) === true)
+    return res.send(response(baseResponse.POSTID_NOTNUM));
 
-    // 해당 유저가 러닝에 속하는지 확인
-    const checkAlreadyapplyNotD = await messageProvider.checkAlreadyapplyNotD(
-        userId,
-        postId
-    );
-    if (checkAlreadyapplyNotD.length === 0) {
-        return res.send(response(baseResponse.USER_NOT_BELONG_RUNNING));
+  // 해당 유저가 러닝에 속하는지 확인
+  const checkAlreadyapplyNotD = await messageProvider.checkAlreadyapplyNotD(
+    userId,
+    postId
+  );
+  if (checkAlreadyapplyNotD.length === 0) {
+    return res.send(response(baseResponse.USER_NOT_BELONG_RUNNING));
+  }
+
+  //jwt로 userId 확인
+  if (userIdFromJWT != userId) {
+    res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
+  } else {
+    // 인증 대기 회원 확인
+    const checkUserAuth = await userProvider.checkUserAuth(userId);
+    if (checkUserAuth.length === 0) {
+      return res.send(response(baseResponse.USER_NON_AUTH));
     }
+    const Response = await runningService.attend(postId, userId);
 
-    //jwt로 userId 확인
-    if (userIdFromJWT != userId) {
-        res.send(errResponse(baseResponse.USER_ID_NOT_MATCH));
-    } else {
-        // 인증 대기 회원 확인
-        const checkUserAuth = await userProvider.checkUserAuth(userId);
-        if (checkUserAuth.length === 0) {
-            return res.send(response(baseResponse.USER_NON_AUTH));
-        }
-        const Response = await runningService.attend(postId, userId);
-
-        return res.send(response(baseResponse.SUCCESS));
-    }
+    return res.send(response(baseResponse.SUCCESS));
+  }
 };
--- src/app/Running/runningRoute.js
+++ src/app/Running/runningRoute.js
@@ -1,24 +1,28 @@
 module.exports = function (app) {
-    const running = require("./runningController");
-    const jwtMiddleware = require("../../../config/jwtMiddleware");
+  const running = require("./runningController");
+  const jwtMiddleware = require("../../../config/jwtMiddleware");
 
-    // 18. 참여 신청 API
-    app.post("/runnings/request/:postId", jwtMiddleware, running.sendRequest);
+  // 18. 참여 신청 API
+  app.post(
+    "/runnings/request/:postId/:userId",
+    jwtMiddleware,
+    running.sendRequest
+  );
 
-    // 19. 참여 신청 처리하기 API
-    app.patch(
-        "/runnings/request/:postId/handling/:applicantId/:whetherAccept",
-        jwtMiddleware,
-        running.handleRequest
-    );
+  // 19. 참여 신청 처리하기 API
+  app.patch(
+    "/runnings/request/:postId/:userId/handling/:applicantId/:whetherAccept",
+    jwtMiddleware,
+    running.handleRequest
+  );
 
-    // 26. 푸시 알림
-    app.get("/push-alarm", jwtMiddleware, running.pushAlarm);
+  // 26. 푸시 알림
+  app.get("/push-alarm", jwtMiddleware, running.pushAlarm);
 
-    // 27. 출석하기 API
-    app.patch(
-        "/runnings/:postId/attendees/:userId",
-        jwtMiddleware,
-        running.attend
-    );
+  // 27. 출석하기 API
+  app.patch(
+    "/runnings/:postId/:userId/attendees",
+    jwtMiddleware,
+    running.attend
+  );
 };
