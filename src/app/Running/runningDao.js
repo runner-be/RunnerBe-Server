ef62d71eb47d0943ec55a07824c4f6f00a7efd2b
great-park <cksgh1735@naver.com>
2022-05-21 00:37:24 +0900

    ⚡️ push alarm 테스트 성공

--- src/app/Running/runningDao.js
+++ src/app/Running/runningDao.js
@@ -1,16 +1,16 @@
 async function sendRequest(connection, sendRequestParams) {
-    const query = `
+  const query = `
   INSERT INTO RunningPeople(gatheringId, userId) 
   VALUES ((SELECT gatheringId FROM Running INNER JOIN Posting P on Running.postId = P.postId WHERE P.postId = ?),?);
                           `;
 
-    const row = await connection.query(query, sendRequestParams);
+  const row = await connection.query(query, sendRequestParams);
 
-    return row;
+  return row;
 }
 
 async function handleRequest(connection, sendRequestParams, whetherAccept) {
-    const query = `
+  const query = `
   UPDATE RunningPeople RP
   INNER JOIN  Running R on RP.gatheringId = R.gatheringId
   INNER JOIN Posting P on R.postId = P.postId
@@ -18,71 +18,68 @@ async function handleRequest(connection, sendRequestParams, whetherAccept) {
   WHERE P.postId = ? AND RP.userId = ?;
                           `;
 
-    const row = await connection.query(query, sendRequestParams);
+  const row = await connection.query(query, sendRequestParams);
 
-    return row;
+  return row;
 }
 
 async function checkApplicant(connection, checkApplicantParams) {
-    const query = `
+  const query = `
   SELECT userId FROM RunningPeople RP
   INNER JOIN Running R on RP.gatheringId = R.gatheringId
   INNER JOIN Posting P on R.postId = P.postId
   WHERE whetherAccept = 'N' AND RP.userId = ? AND P.postId = ?
                           `;
 
-    const row = await connection.query(query, checkApplicantParams);
+  const row = await connection.query(query, checkApplicantParams);
 
-    return row;
+  return row;
 }
 
 //디바이스 토큰 가져오기
-async function getDeviceToken(userId) {
-    const connection = await pool.getConnection(async (conn) => conn);
-    const getDeviceTokenQuery = `
+async function getDeviceToken(connection, userId) {
+  const getDeviceTokenQuery = `
       select deviceToken, nickName
       from User
       where userId = ?
   `;
-    const [getDeviceTokenRows] = await connection.query(
-        getDeviceTokenQuery,
-        userId
-    );
-    connection.release();
-
-    return getDeviceTokenRows;
+  const [getDeviceTokenRows] = await connection.query(
+    getDeviceTokenQuery,
+    userId
+  );
+  return getDeviceTokenRows;
 }
 
 // RP에 참석여부 업데이트
 async function updateR(connection, updateParams) {
-    const query = `
+  const query = `
   UPDATE RunningPeople
   SET attendance = 1
   WHERE userId = ? AND gatheringId = (SELECT gatheringId FROM Running WHERE postId = ?);
                           `;
 
-    const row = await connection.query(query, updateParams);
+  const row = await connection.query(query, updateParams);
 
-    return row;
+  return row;
 }
 
 // 유저의 출석률 업데이트
 async function updateU(connection, updateParamsU) {
-    const query = `
+  const query = `
   UPDATE User
   SET diligence = (SELECT (SUM(attendance)/COUNT(gatheringId))*100 as attendance FROM RunningPeople WHERE userId = ? GROUP BY userId)
   WHERE userId = ?
                           `;
 
-    const row = await connection.query(query, updateParamsU);
+  const row = await connection.query(query, updateParamsU);
 
-    return row;
+  return row;
 }
 module.exports = {
-    sendRequest,
-    handleRequest,
-    checkApplicant,
-    getDeviceToken,
-    updateR,
-    updateU,
+  sendRequest,
+  handleRequest,
+  checkApplicant,
+  getDeviceToken,
+  updateR,
+  updateU,
 };
