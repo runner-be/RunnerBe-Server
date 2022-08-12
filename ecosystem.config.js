117215add446c82a2cc648c4e4e2fee7fe4beea5
great-park <cksgh1735@naver.com>
2022-08-12 18:23:13 +0900

    🎨 pm2 cluster config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       script: "./index.js",
       instances: 0,
       exec_mode: "cluster",
+      wait_ready: true, // load하기 전에 ready
+      kill_timeout: 5000, // SIGINT -> SIGKILL 전송까지의 시간
     },
   ],
 };
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('./config/express');
-const {logger} = require('./config/winston');
+const express = require("./config/express");
+const { logger } = require("./config/winston");
 
 const port = 3000;
-express().listen(port);
+
+process.on("SIGINT", async () => {
+  await express().close();
+  process.exit(0);
+});
+
+express().listen(port, () => {
+  process.send("ready"); //ready 전달
+});
+
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
