505f2d5a513fd0521091d631618dbfdb795c2732
great-park <cksgh1735@naver.com>
2022-08-12 18:36:16 +0900

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,10 @@ module.exports = {
     {
       name: "runnerbeApp",
       script: "./index.js",
-      instances: 0,
+      instances: max,
       exec_mode: "cluster",
       wait_ready: true, // load하기 전에 ready
+      listen_timeout: 50000,
       kill_timeout: 5000, // SIGINT -> SIGKILL 전송까지의 시간
     },
   ],
