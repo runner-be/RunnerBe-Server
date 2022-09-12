a9cf755c373e935181c10ecc3ce657594df9c9d2
great-park <cksgh1735@naver.com>
2022-09-12 18:14:01 +0900

    edit: config database

--- config/database.js
+++ config/database.js
@@ -10,11 +10,6 @@ const pool = mysql.createPool({
   port: "3306",
   password: DBpassword,
   database: DBschema,
-  waitForConnections: true,
-  connectionLimit: 100,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 10000,
 });
 
 module.exports = {
